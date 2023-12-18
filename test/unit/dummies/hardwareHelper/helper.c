8ae83883261e13d114d8430b25d657761dc52cb8
Renhong Liu <1226912906@qq.com>
2023-12-19 03:54:09 +0800

    test(unit test hardwareHelper): test more functions in hardwareHelper.c

--- test/unit/dummies/hardwareHelper/helper.c
+++ test/unit/dummies/hardwareHelper/helper.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdlib.h>
 #include "helper.h"
 
 const char *allowedCharacters = "0123456789i;\n";
@@ -18,4 +19,28 @@ bool isLineEnd(char charToCheck) {
 
 bool isMessageToLong(size_t numberOfCharacters, size_t bufferLength) {
     return numberOfCharacters >= bufferLength - 1;
+}
+
+void initializeMessageHandler(char **buffer, size_t bufferLength, size_t *characterCounter) {
+    *buffer = malloc(bufferLength);
+    resetMessageBuffer(*buffer, bufferLength, characterCounter);
+}
+
+void resetMessageBuffer(char *buffer, size_t bufferSize, size_t *receivedCharacterCount) {
+    memset(buffer, '\0', bufferSize);
+    *receivedCharacterCount = 0;
+}
+
+uint32_t parseInputString(char **message) {
+    // every halt timing command has to end with a ';'
+    // the function will search for this char and save its position
+    char *semicolonPosition = strchr(*message, ';');
+    if (semicolonPosition == NULL) {
+        return 0; // No Semicolon found
+    }
+    // the string will be cast, from the beginning of the string to the
+    // ';'-Position, into an integer
+    uint32_t delay = strtol(*message, &semicolonPosition, 10);
+    *message = semicolonPosition + 1;
+    return delay;
 }
