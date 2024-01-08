ecea51692754aeabfa8ed32a432bf4adc31a8b77
Renhong Liu <1226912906@qq.com>
2024-01-09 05:00:10 +0800

    test(unit test dispenser): test the cycle of a working dispenser.

---
+++ test/unit/dummies/limitSwitch/limitSwitch.c
@@ -0,0 +1,35 @@
+#define SOURCE_FILE "LIMIT_SWITCH"
+
+#include "limitSwitch.h"
+#include <hardware/gpio.h>
+
+bool re = true;
+bool limitSwitchIsClosed(limitSwitch_t limitSwitch) {
+    re = !re;
+    return re;
+}
+
+limitSwitch_t createLimitSwitch(uint8_t id) {
+    limitSwitch_t limitSwitch;
+
+    switch (id) {
+    case 0:
+        limitSwitch = LIMIT_SWITCH_0;
+        break;
+    case 1:
+        limitSwitch = LIMIT_SWITCH_1;
+        break;
+    case 2:
+        limitSwitch = LIMIT_SWITCH_2;
+        break;
+    case 3:
+        limitSwitch = LIMIT_SWITCH_3;
+        break;
+    default:
+        return (limitSwitch_t){.pin = -1};
+    }
+
+    gpio_init(limitSwitch.pin);
+    gpio_set_dir(limitSwitch.pin, GPIO_IN);
+    return limitSwitch;
+}
